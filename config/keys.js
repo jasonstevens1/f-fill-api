c1077722883ed3941acbb1cfdf2b8b662b2dfc20
Jason Stevens <jason.stevens@shaw.ca>
2019-01-03 13:35:44 -0700

    Implement Passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://ffilluser:sd67GD*d234@ds119323.mlab.com:19323/f-fill-api"
+  mongoURI:
+    "mongodb://ffilluser:sd67GD*d234@ds119323.mlab.com:19323/f-fill-api",
+  secretOrKey: "somethang"
 };
